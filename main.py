0a12e783bde0ad01554bde617cf1069bf1d44c4e
Shabrashin <IvanShabrashin@gmail.com>
2022-05-13 12:55:53 +0300

    Поправил README и добавил регулярную отправку сообщений

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import asyncio
 import telegram
-from random import choice
 import os
 from dotenv import load_dotenv
+import time
 
 
 async def main():
     load_dotenv()
     TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
     TELEGRAM_CHANNEL_ID = os.getenv('TELEGRAM_CHANNEL_ID')
-    
-    bot = telegram.Bot(TELEGRAM_TOKEN)    
-    bot.send_photo(chat_id=TELEGRAM_CHANNEL_ID, photo=open(f'images/{choice(os.listdir("images"))}', 'rb'))
+    SENDING_FREQUENCY = os.getenv('SENDING_FREQUENCY')
+
+    bot = telegram.Bot(TELEGRAM_TOKEN)  
+    for root, dirs, files in os.walk('images'):
+        for filename in files:
+            bot.send_photo(chat_id=TELEGRAM_CHANNEL_ID, photo=open(f'images/{filename}', 'rb'))
+            time.sleep(int(SENDING_FREQUENCY))
 
 
 if __name__ == '__main__':
